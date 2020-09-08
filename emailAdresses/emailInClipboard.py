03946c875bc2994b10a4021cbb51df5a2f3a9670
Emil Merle <emilmerle8@gmail.com>
2020-09-08 19:59:00 +0200

    Added emailAdresses/emailInClipboard.py

---
+++ emailAdresses/emailInClipboard.py
@@ -0,0 +1,28 @@
+#! python3
+# emailInClipboard.py - Finds email addresses on the clipboard and copies them to the clipboard
+# Usage: call the program
+# Example: >>> numberOnClipboard.py
+
+import pyperclip, re, sys
+
+# Create email regex.
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @ # @ symbol
+    [a-zA-Z0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot-something
+    )''', re.VERBOSE)
+
+# Find matches in clipboard text.
+text = str(pyperclip.paste())
+matches = []
+for groups in emailRegex.findall(text):
+    emails = groups[0]
+    matches.append(emails)
+
+if len(matches) > 0:
+    pyperclip.copy(' -+- '.join(matches))
+    print("Email address(es) found:")
+    print('\n'.join(matches))
+else:
+    print('No email addresses found.')
