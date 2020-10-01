a2690db9edc36063204aba909c0382b993a438aa
Ian S. Pringle <ipringle2@liberty.edu>
2020-10-01 05:52:52 -0400

    Cleanup of email dir
    - Added the `if __name__ ...` pattern
    - moved email regexp to it's own file for importing from both other
      files
    - appeased the linter

--- emailAdresses/emailInClipboard.py
+++ emailAdresses/emailInClipboard.py
@@ -3,26 +3,25 @@
 # Usage: Copy some text call the program
 # Example: >>> numberOnClipboard.py
 
-import pyperclip, re, sys
+import email
+import pyperclip
 
-# Create email regex.
-emailRegex = re.compile(r'''(
-    [a-zA-Z0-9._%+-]+ # username
-    @ # @ symbol
-    [a-zA-Z0-9.-]+ # domain name
-    (\.[a-zA-Z]{2,4}) # dot-something
-    )''', re.VERBOSE)
 
-# Find matches in clipboard text.
-text = str(pyperclip.paste())
-matches = []
-for groups in emailRegex.findall(text):
-    emails = groups[0]
-    matches.append(emails)
+def main():
+    # Find matches in clipboard text.
+    text = str(pyperclip.paste())
+    matches = []
+    for groups in email.regexp.findall(text):
+        emails = groups[0]
+        matches.append(emails)
 
-if len(matches) > 0:
-    pyperclip.copy(' -+- '.join(matches))
-    print("Email address(es) found:")
-    print('\n'.join(matches))
-else:
-    print('No email address found.')
+    if len(matches) > 0:
+        pyperclip.copy(' -+- '.join(matches))
+        print("Email address(es) found:")
+        print('\n'.join(matches))
+    else:
+        print('No email address found.')
+
+
+if __name__ == "__main__":
+    main()
--- emailAdresses/emailInFile.py
+++ emailAdresses/emailInFile.py
@@ -3,50 +3,52 @@
 # Usage: call the program with a file path as the first argument
 # Example: >>> emailInFile.py C:\\Users\\ExampleUser\\Documents\\addresses.txt
 
-import pyperclip, re, sys, os
+import email
+import pyperclip
+import sys
+import os
 
-#Check if sys.argv[1] is given
-argumentOne = ""
-try:
-    argumentOne = sys.argv[1]
-except IndexError:
-    exit("No second argument given. Try again with a file path as an argument")
 
-# Make absolute path if given path is a relative path
-if(not os.path.isabs(argumentOne)):
-    argumentOne = os.path.abspath(argumentOne)
-
-openedContent = ""
-
-# Check if argumentOne is a valid path and file
-if(os.path.exists(argumentOne) and os.path.isfile(argumentOne)):
-    # Try to open and read file
+def main():
+    # Check if sys.argv[1] is given
+    argumentOne = ""
     try:
-        openedFile = open(argumentOne, "r")
-        openedContent = openedFile.read()
-    except OSError:
-        exit("Could not open file")
-else:
-    exit("Given file does not exist: " + argumentOne)
+        argumentOne = sys.argv[1]
+    except IndexError:
+        exit("No second argument given. "
+             "Try again with a file path as an argument")
 
-# Create email regex.
-emailRegex = re.compile(r'''(
-    [a-zA-Z0-9._%+-]+ # username
-    @ # @ symbol
-    [a-zA-Z0-9.-]+ # domain name
-    (\.[a-zA-Z]{2,4}) # dot-something
-    )''', re.VERBOSE)
+    # Make absolute path if given path is a relative path
+    if(not os.path.isabs(argumentOne)):
+        argumentOne = os.path.abspath(argumentOne)
 
-matches = []
-for groups in emailRegex.findall(openedContent):
-    emails = groups[0]
-    matches.append(emails)
+    openedContent = ""
 
-openedFile.close()
+    # Check if argumentOne is a valid path and file
+    if(os.path.exists(argumentOne) and os.path.isfile(argumentOne)):
+        # Try to open and read file
+        try:
+            openedFile = open(argumentOne, "r")
+            openedContent = openedFile.read()
+        except OSError:
+            exit("Could not open file")
+    else:
+        exit("Given file does not exist: " + argumentOne)
 
-if len(matches) > 0:
-    pyperclip.copy(' -+- '.join(matches))
-    print("Email address(es) found:")
-    print('\n'.join(matches))
-else:
-    print('No email address found.')
+    matches = []
+    for groups in email.regexp.findall(openedContent):
+        emails = groups[0]
+        matches.append(emails)
+
+    openedFile.close()
+
+    if len(matches) > 0:
+        pyperclip.copy(' -+- '.join(matches))
+        print("Email address(es) found:")
+        print('\n'.join(matches))
+    else:
+        print('No email address found.')
+
+
+if __name__ == "__main__":
+    main()
